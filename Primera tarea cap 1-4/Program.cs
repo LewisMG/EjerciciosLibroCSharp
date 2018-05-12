e3863486d0f3b3fcbb879be789027283e7d67bec
Lewis Mendoza <Lewis_mendoza@ucne.edu.do>
2018-05-12 16:25:34 -0400

    Add project files.

---
+++ Primera tarea cap 1-4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primera_tarea_cap_1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
